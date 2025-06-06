521061b2c983cdd839a62c8a51a19f0361ab02be
vcaina <vcaina@rosemet.com>
2025-06-06 10:13:45 -0500

    Add named ranking system

--- rank_system/src/endstone_rank_system/rank_system.py
+++ rank_system/src/endstone_rank_system/rank_system.py
@@ -9,7 +9,7 @@ from endstone.event import (
 )
 from endstone.form import ActionForm
 from endstone.plugin import Plugin
-from endstone.scoreboard import Criteria, DisplaySlot
+from endstone.scoreboard import Criteria
 
 
 class RankSystem(Plugin):
@@ -31,6 +31,24 @@ class RankSystem(Plugin):
         }
     }
 
+    RANKS = {
+        "mob_kills": [
+            (0, "Hunter"),
+            (10, "Slayer"),
+            (50, "Beastmaster"),
+        ],
+        "player_kills": [
+            (0, "Fighter"),
+            (10, "Warrior"),
+            (30, "Champion"),
+        ],
+        "ores_mined": [
+            (0, "Miner"),
+            (50, "Excavator"),
+            (150, "Prospector"),
+        ],
+    }
+
     ORES = {
         "minecraft:coal_ore",
         "minecraft:iron_ore",
@@ -44,6 +62,10 @@ class RankSystem(Plugin):
         "minecraft:ancient_debris",
     }
 
+    def __init__(self):
+        super().__init__()
+        self._selected: dict[str, str] = {}
+
     def on_enable(self) -> None:
         sb = self.server.scoreboard
         for obj, display in [
@@ -56,6 +78,25 @@ class RankSystem(Plugin):
 
         self.register_events(self)
 
+    def _get_rank_name(self, obj_name: str, value: int) -> str:
+        tiers = self.RANKS.get(obj_name, [])
+        rank = tiers[0][1] if tiers else ""
+        for threshold, name in tiers:
+            if value >= threshold:
+                rank = name
+            else:
+                break
+        return rank
+
+    def _update_player_rank(self, player) -> None:
+        stat = self._selected.get(player.unique_id)
+        if not stat:
+            return
+        obj = self.server.scoreboard.get_objective(stat)
+        score = obj.get_score(player)
+        rank_name = self._get_rank_name(stat, score.value)
+        player.name_tag = f"[{rank_name}] {player.name}"
+
     # Event handlers
     @event_handler
     def on_actor_death(self, event: ActorDeathEvent) -> None:
@@ -64,6 +105,8 @@ class RankSystem(Plugin):
             obj = self.server.scoreboard.get_objective("mob_kills")
             score = obj.get_score(killer)
             score.value = score.value + 1
+            if self._selected.get(killer.unique_id) == "mob_kills":
+                self._update_player_rank(killer)
 
     @event_handler
     def on_player_death(self, event: PlayerDeathEvent) -> None:
@@ -72,6 +115,8 @@ class RankSystem(Plugin):
             obj = self.server.scoreboard.get_objective("player_kills")
             score = obj.get_score(killer)
             score.value = score.value + 1
+            if self._selected.get(killer.unique_id) == "player_kills":
+                self._update_player_rank(killer)
 
     @event_handler
     def on_block_break(self, event: BlockBreakEvent) -> None:
@@ -79,13 +124,16 @@ class RankSystem(Plugin):
             obj = self.server.scoreboard.get_objective("ores_mined")
             score = obj.get_score(event.player)
             score.value = score.value + 1
+            if self._selected.get(event.player.unique_id) == "ores_mined":
+                self._update_player_rank(event.player)
 
     @event_handler
     def on_player_join(self, event: PlayerJoinEvent) -> None:
         # Display mob_kills by default
         obj = self.server.scoreboard.get_objective("mob_kills")
-        obj.set_display(DisplaySlot.SIDEBAR)
         event.player.scoreboard = self.server.scoreboard
+        self._selected[event.player.unique_id] = "mob_kills"
+        self._update_player_rank(event.player)
 
     def on_command(self, sender, command, args):
         if command.name != "rank" or not sender.is_player:
@@ -100,10 +148,9 @@ class RankSystem(Plugin):
 
         def handle(p, index):
             obj_name = ["mob_kills", "player_kills", "ores_mined"][index]
-            obj = sb.get_objective(obj_name)
-            sb.clear_slot(DisplaySlot.SIDEBAR)
-            obj.set_display(DisplaySlot.SIDEBAR)
+            self._selected[p.unique_id] = obj_name
             p.scoreboard = sb
+            self._update_player_rank(p)
 
         form.on_submit = handle
         form.show(player)
